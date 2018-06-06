6d4ae60246fb21273e5d4a085bd7b1a8bbac23b1
lazyman123 <875915286@qq.com>
2018-06-06 20:26:17 +0800

    fix unknow bug

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable.styl"
+  #app
+    color $color-theme
+</style>
