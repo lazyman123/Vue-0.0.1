bbc355f18d8a6263d0c671b142ece0e5bc7ef9eb
lazyman123 <875915286@qq.com>
2018-06-18 04:48:04 +0800

    将歌曲详情数据封装成Song类

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,11 +6,15 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import singerVue from '../singer/singer.vue';
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
-
+import { createSong } from 'common/js/song'
 export default {
+  data () {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
@@ -28,9 +32,20 @@ export default {
       }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.data && res.code === ERR_OK) {
-          console.log(res.data)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
     }
   }
 }
