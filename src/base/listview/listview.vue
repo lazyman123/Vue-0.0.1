8ecbbba996ae1873d7bc43358d713a1f443e2037
lazyman123 <875915286@qq.com>
2018-06-17 17:39:53 +0800

    添加歌手列表组件，实现列表滚动和右侧字母表的联动

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,31 +1,135 @@
 <template>
-  <scroll class="listview" :data="data">
+  <scroll class="listview" 
+          :data="data" 
+          :listenScroll="listenScroll" 
+          ref="listview"
+          :probeType="probeType"
+          @scroll="scroll">
     <ul>
-      <li v-for="(group, index) in data" :key="index" class="list-group">
+      <li v-for="(group, index) in data" :key="index" class="list-group" ref="listgroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
           <li v-for="item in group.items" :key="item.id" class="list-group-item">
-            <img v-lazy="item.avatar" class="avatar" alt="">
+            <img v-lazy="item.avatar" class="avatar">
             <span class="name">{{item.name}}</span>
           </li>
         </ul>
       </li>
     </ul>
+    <div class="list-shortcut" 
+         @touchstart="onShortcutTouchStart" 
+         @touchmove.stop.prevent="onShortcutTouchMove">
+      <ul>
+        <li v-for="(item, index) in shortcutList" 
+            class="item" 
+            :key="index" 
+            :data-index="index" 
+            :class="{'current': currentIndex === index}">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+    <div class="loading-container" v-show="!data.length">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import { getData } from 'common/js/dom'
+
+const ANCHOR_HEIGHT = 18
 
 export default {
+  created () {
+    this.touch = {}
+    this.listenScroll = true
+    this.listHeight = []
+    this.probeType = 3
+  },
+  data () {
+    return {
+      scrollY: -1,
+      currentIndex: 0
+    }
+  },
   props: {
     data: {
       type: Array,
       default: []
     }
   },
+  computed: {
+    shortcutList () {
+      return this.data.map((group) => {
+        return group.title.substr(0,1)
+      })
+    }
+  },
+  watch: {
+    data () {
+      this.$nextTick(() => {
+        this._calculateHeight()
+      })
+    },
+    scrollY (newY) {
+      const listHeight = this.listHeight
+      // 当滚动到顶部，newY>0
+      if (newY > 0) {
+        this.currentIndex = 0
+        return
+      }
+      // 在中间部分滚动
+      for (let i = 0; i < listHeight.length - 1; i++) {
+        let height1 = listHeight[i]
+        let height2 = listHeight[i+1]
+        if (-newY < height2 && -newY >= height1) {
+          this.currentIndex = i
+          return
+        }
+      }
+      this.currentIndex = listHeight - 2
+    }
+  },
+  methods: {
+    onShortcutTouchStart (e) {
+      let anchorIndex = getData(e.target, 'index')
+      let firstTouch = e.touches[0]
+      this.touch.y1 = firstTouch.pageY
+      this.touch.anchorIndex = anchorIndex
+      this._scrollTo(anchorIndex)
+    },
+    onShortcutTouchMove (e) {
+      let firstTouch = e.touches[0]
+      this.touch.y2 = firstTouch.pageY
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
+      let anchorIndex = +this.touch.anchorIndex + delta
+      this._scrollTo(anchorIndex)
+    },
+    scroll (pos) {
+      this.scrollY = pos.y
+    },
+    _scrollTo (index) {
+      this.$refs.listview.scrollToElement(this.$refs.listgroup[index], 0)
+      // this.scrollY = this.$refs.listview.scroll.y
+    },
+    _calculateHeight () {
+      this.listHeight = []
+      const list = this.$refs.listgroup
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < list.length; i++) {
+        let item = list[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    }
+  },
   components: {
-    Scroll
+    Scroll,
+    Loading
   }
 }
 </script>
